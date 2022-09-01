batmanscode/pydacc

commit 4cd0e4227eb7a19e6200b251d52e31e94c782fcc

Batmanscode railway 1 (#11)
* add gunicorn 20.1.0

* Create deploy.template.yaml

* Create gunicorn.conf.py

* Create Procfile

* add digital ocean deploy button

* update how to run locally

* moved `name` up

* move everything under `spec`

* Update deploy.template.yaml

changed to use `git` instead of `github` since there was a github authentication error when using the deploy button

* upgrade klib from 2.0 to 2.3

* scikit-learn==0.23.2 to fix the deployment problem

pycaret needed this

* move scikit-learn to the top

* Add missing datetime, assign_cluster_labels imports

* Update requirements.txt

* klib (1.0.1) no longer uses scikit-learn
* scikit-learn removed since pycaret will install it. pycaret and klib both used scikit-learn and there were version issues sometimes so it was used as a temporary work around

* Delete .do directory

* Delete Procfile

* Delete gunicorn.conf.py

* remove do button and gunicorn command

* Create Procfile

for railway.app

* Delete Procfile

revert last commit

* Create Procfile

for railway.app

* Update Procfile

forgot to add "web"

* Update requirements.txt

railway build failed and was missing python-multipart

* Update Procfile

* don't specify port
* railway built successfully but there was a port issue

* Update Procfile

railway.app needs apps to have a $PORT set as a variable

* Update Procfile

Error: Got unexpected extra arguments (port 8080)

* Update Procfile

this seems like the correct way to set a variable in uvicorn

* Update Procfile

UVICORN_PORT doesn't have to be set, any environment variable under that will be passed to port

* Update Procfile

changes didn't work - going back to the full command without any variables

* Update Procfile

got help from discord, trying again with $PORT and deleting variables from the railway.app menu

* temporarily added cors middleware

api.py
@@ -1,12 +1,15 @@
 import shutil
-from typing import List
+from typing import List, Optional
 import pandas as pd
-from fastapi import FastAPI, File, Query, UploadFile, Form, BackgroundTasks
+from fastapi import FastAPI, File, Query, UploadFile, Form, BackgroundTasks, Body
 from fastapi.responses import FileResponse, PlainTextResponse
+from pydantic import BaseModel, Field
 from starlette.background import BackgroundTask
 import os
+from datetime import date
+from pydacc.clustering import train_clustering_model, automl_clustering, predict_cluster_label , assign_cluster_labels
 
-from pydacc.clustering import train_clustering_model, automl_clustering, predict_cluster_label
+from fastapi.middleware.cors import CORSMiddleware # temp for cors
 
 
 request_example = """
@@ -49,6 +52,17 @@ app = FastAPI(
     title="AutoML 🤖: Clustering", description=request_example, version="1.0.0"
 )
 
+# TEMP FOR CORS
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 def save_to_disk(uploaded_file):
     """
@@ -87,6 +101,8 @@ Note: add "/docs" to the url to get the swagger ui docs or "/redoc"
 
 # for each parameter
 # having them here since they share most of it
+path_to_csv_doc = "Path to a `.csv` file. Alternatively, can be sent as a string"
+
 k_doc = "number of clusters to create"
 
 column_drop_threshold_doc = """
@@ -124,17 +140,17 @@ Calls the `train_clustering_model` internally.
 """
 
 
-@app.post("/clustering/", description=clustering_doc)
+@app.post("/clustering", description=clustering_doc)
 def clustering(
     background_tasks: BackgroundTasks,
-    path_to_csv: UploadFile = File(...),
+    path_to_csv: UploadFile = File(..., description=path_to_csv_doc),
     k: int = Form(..., description=k_doc),
     column_drop_threshold: float = Form(0.99, description=column_drop_threshold_doc),
     file_name: str = Form("cluster_model", description=file_name_doc),
-    drop_columns: List[str] = Form(None, description=drop_columns_doc),
-    categorical_columns: List[str] = Form(None, description=categorical_columns_doc),
-    numerical_columns: List[str] = Form(None, description=numerical_columns_doc),
-    ignore_features: List[str] = Form(None, description=ignore_features_doc),
+    drop_columns: Optional[List[str]] = Form(None, description=drop_columns_doc),
+    categorical_columns: Optional[List[str]] = Form(None, description=categorical_columns_doc),
+    numerical_columns: Optional[List[str]] = Form(None, description=numerical_columns_doc),
+    ignore_features: Optional[List[str]] = Form(None, description=ignore_features_doc),
     output_format: str = Form("csv", description=output_format_doc),
 ):
 
@@ -172,16 +188,16 @@ def clustering(
     return FileResponse(cluster_labels, background=BackgroundTask(delete_temp))
 
 
-@app.post("/auto-clustering/", description=auto_clustering_doc)
+@app.post("/auto-clustering", description=auto_clustering_doc)
 def auto_clustering(
     background_tasks: BackgroundTasks,
-    path_to_csv: UploadFile = File(...),
+    path_to_csv: UploadFile = File(..., description=path_to_csv_doc),
     column_drop_threshold: float = Form(0.99, description=column_drop_threshold_doc),
     file_name: str = Form("cluster_model", description=file_name_doc),
-    drop_columns: List[str] = Form(None, description=drop_columns_doc),
-    categorical_columns: List[str] = Form(None, description=categorical_columns_doc),
-    numerical_columns: List[str] = Form(None, description=numerical_columns_doc),
-    ignore_features: List[str] = Form(None, description=ignore_features_doc),
+    drop_columns: Optional[List[str]] = Form(None, description=drop_columns_doc),
+    categorical_columns: Optional[List[str]] = Form(None, description=categorical_columns_doc),
+    numerical_columns: Optional[List[str]] = Form(None, description=numerical_columns_doc),
+    ignore_features: Optional[List[str]] = Form(None, description=ignore_features_doc),
     output_format: str = Form("csv", description=output_format_doc),
 ):
 

pydacc/data_cleaning.py
@@ -13,6 +13,7 @@ def clean_data(
     >>> data = clean_data('raw_data.csv', drop_threshold=0.9)
 
     path_to_csv: str
+      Path to a '.csv` file.
 
     drop_columns: str, list, default=None
       Columns that aren't needed.
@@ -26,8 +27,30 @@ def clean_data(
 
     import pandas as pd
     import klib
+    from io import StringIO
+    
+#     # if the csv input is a string, wrap in the StringIO function
+#     if isinstance(path_to_csv, str):
+#         try:
+#             path_to_csv = StringIO(path_to_csv)
+#         except:
+#             raise ValueError("you sent a weird string that couldn't be parsed by StringIO")
+#     else:
+#         pass
 
-    df = pd.read_csv(path_to_csv)
+#     df = pd.read_csv(path_to_csv)
+    
+    # take csv input from string or file and convert to pandas dataframe
+    try:
+        df = pd.read_csv(path_to_csv)
+        print("Pandas Dataframe created from a CSV file")
+    except:
+        df = pd.read_csv(StringIO(path_to_csv))
+        print("Pandas Dataframe created from a CSV string")
+    
+    # check if converted to dataframe
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("data passed couldn't be converted to a Pandas Dataframe")
 
     # drop named columns
     if drop_columns:

Procfile
@@ -1,0 +1,1 @@
+web: uvicorn api:app --host 0.0.0.0 --port $PORT

commit edaea85f35325292e0347fca812b634dd1cfa337

Fix for not being able to send `None` to optional params (#12)
* add none or string to optional params

* fastapi==0.70.0 to fastapi==0.81.0

* update starlette to fix fastapi dependancy error

```
#11 7.530 ERROR: Cannot install -r requirements.txt (line 1) and starlette==0.16.0 because these package versions have conflicting dependencies.
#11 7.531
#11 7.531 The conflict is caused by:
#11 7.531     The user requested starlette==0.16.0
#11 7.531     fastapi 0.81.0 depends on starlette==0.19.1
```

* fix syntax

* fix typos

* use `Union` instead of `|`

* import `Union`

* fix for being able to send `None` to optional params

turns out the problem was: `TypeError: argument of type 'NoneType' is not iterable`

`get_common_items()` crashed when one of its arguments was `None` which is what happens when an optional param is empty - it needed to return `None` instead

api.py
@@ -1,5 +1,5 @@
 import shutil
-from typing import List, Optional
+from typing import List, Optional, Union
 import pandas as pd
 from fastapi import FastAPI, File, Query, UploadFile, Form, BackgroundTasks, Body
 from fastapi.responses import FileResponse, PlainTextResponse
@@ -147,10 +147,10 @@ def clustering(
     k: int = Form(..., description=k_doc),
     column_drop_threshold: float = Form(0.99, description=column_drop_threshold_doc),
     file_name: str = Form("cluster_model", description=file_name_doc),
-    drop_columns: Optional[List[str]] = Form(None, description=drop_columns_doc),
-    categorical_columns: Optional[List[str]] = Form(None, description=categorical_columns_doc),
-    numerical_columns: Optional[List[str]] = Form(None, description=numerical_columns_doc),
-    ignore_features: Optional[List[str]] = Form(None, description=ignore_features_doc),
+    drop_columns: Union[List[str], None] = Form(None, description=drop_columns_doc),
+    categorical_columns: Union[List[str], None] = Form(None, description=categorical_columns_doc),
+    numerical_columns: Union[List[str], None] = Form(None, description=numerical_columns_doc),
+    ignore_features: Union[List[str], None] = Form(None, description=ignore_features_doc),
     output_format: str = Form("csv", description=output_format_doc),
 ):
 
@@ -194,10 +194,10 @@ def auto_clustering(
     path_to_csv: UploadFile = File(..., description=path_to_csv_doc),
     column_drop_threshold: float = Form(0.99, description=column_drop_threshold_doc),
     file_name: str = Form("cluster_model", description=file_name_doc),
-    drop_columns: Optional[List[str]] = Form(None, description=drop_columns_doc),
-    categorical_columns: Optional[List[str]] = Form(None, description=categorical_columns_doc),
-    numerical_columns: Optional[List[str]] = Form(None, description=numerical_columns_doc),
-    ignore_features: Optional[List[str]] = Form(None, description=ignore_features_doc),
+    drop_columns: Union[List[str], None] = Form(None, description=drop_columns_doc),
+    categorical_columns: Union[List[str], None] = Form(None, description=categorical_columns_doc),
+    numerical_columns: Union[List[str], None] = Form(None, description=numerical_columns_doc),
+    ignore_features: Union[List[str], None] = Form(None, description=ignore_features_doc),
     output_format: str = Form("csv", description=output_format_doc),
 ):
 

pydacc/data_cleaning.py
@@ -82,6 +82,7 @@ def clean_data(
 def get_common_items(list1, list2):
     """
     Compares two lists and returns the common items in a new list. Used internally.
+    If one of the lists is `None` it will return `None` instead of trying to iterate through eeach. This prevents the `TypeError: argument of type 'NoneType' is not iterable` error.
 
     Example
     -------
@@ -95,5 +96,8 @@ def get_common_items(list1, list2):
 
     returns: list
     """
-    common = [value for value in list1 if value in list2]
-    return common
+    if list1 or list2 is None:
+        return None
+    else:
+        common = [value for value in list1 if value in list2]
+        return common